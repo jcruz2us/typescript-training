64adcfab4c0887d49a3377b49e67e512983a60e8
Jorge Cruz <jorge@hipmunk.com>
2019-02-07 12:56:34 -0800

    add city to hotel

--- src/examples/3.classes.js
+++ src/examples/3.classes.js
@@ -1,11 +1,12 @@
 export class Hotel {
-  constructor(name, description, stars) {
+  constructor(name, description, stars, city) {
     this.name = name;
     this.description = description;
     this.stars = stars;
+    this.city = city;
   }
 }
 
 (function run() {
-  const hotel = new Hotel(1, false, {});
+  const hotel = new Hotel(1, false, {}, null);
 })();
