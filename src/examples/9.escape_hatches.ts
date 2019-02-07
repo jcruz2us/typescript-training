4065fa1aa7bd741a79d43194a908cfbb52668007
Jorge Cruz <jorge@hipmunk.com>
2019-02-07 12:56:20 -0800

    allow unused locals

--- src/examples/9.escape_hatches.ts
+++ src/examples/9.escape_hatches.ts
@@ -1,26 +1,24 @@
-export class Hotel {
-  constructor(public name?: string) {}
-}
+import {Hotel} from "./3.classes";
 
 (function run() {
   const result = {
     foo: 1,
   };
-  const anyResult = result as any;
-  anyResult.car = 1;
+  // result.car = 2;
+
+  // const anyResult = result as any;
+  // anyResult.car = 1;
 
   const hotel = new Hotel("Foo");
+  // const name = hotel.city.name;
   // non-null assertion operator = !
-  console.log(hotel.name!.length);
+  // console.log(hotel.city!.length);
 
-  if (hotel.name) {
-    console.log(hotel.name.length);
-  }
+  // if (hotel.name) {
+  //   console.log(hotel.name.length);
+  // }
 
   // cast to any
-  console.log((hotel.name as any).length);
+  // console.log((hotel.name as any).length);
 
-  // as any
-  // const foo = {} as any;
-  // foo.length;
 })();
